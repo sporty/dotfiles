fe3f485c6a5a39bf103d56c6f5919ed5392c8634
root <root@vita-local.(none)>
2012-03-01 16:17:32 +0900

    linux用セットアップスクリプト追加

---
+++ setup_linux.sh
@@ -0,0 +1,21 @@
+#!/bin/sh 
+
+# ドットファイルのシンボリックリンクを作成
+if [ ! -d ~/vimfiles ]; then
+	ln -s ~/dotfiles/vimfiles ~/vimfiles
+fi
+
+if [ ! -f ~/.vimrc ]; then
+	ln -s ~/dotfiles/_vimrc ~/.vimrc
+fi
+if [ ! -f ~/.gvimrc ]; then
+	ln -s ~/dotfiles/_gvimrc ~/.gvimrc
+fi
+if [ ! -f ~/.bashrc ]; then
+	ln -s ~/dotfiles/_bashrc ~/.bashrc
+fi
+if [ ! -f ~/.profile ]; then
+	ln -s ~/dotfiles/_profile ~/.profile
+fi
+
+# EOF
