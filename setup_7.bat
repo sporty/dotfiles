aa1bc227949d1c0e46b88bd51517385c0adda1a8
Ryo Takahashi <rt.sporty@gmail.com>
2013-04-16 01:36:42 +0900

    セットアップ時にシンボリックリンクを一旦削除するようにした

--- setup_7.bat
+++ setup_7.bat
@@ -5,6 +5,7 @@ rem gitの設定
 rem ======================================================================
 
 rem シンボリックリンクの作成
+del "%HOME%\.gitignore_global"
 mklink /H "%HOME%\.gitignore_global" "%HOME%\dotfiles\git\_gitignore_global"
 
 rem ======================================================================
@@ -12,6 +13,9 @@ rem bashの設定
 rem ======================================================================
 
 rem シンボリックリンクの作成
+del "%HOME%\.bashrc"
+del "%HOME%\.profile"
+del "%HOME%\.inputrc"
 mklink /H "%HOME%\.bashrc" "%HOME%\dotfiles\bash\_bashrc"
 mklink /H "%HOME%\.profile" "%HOME%\dotfiles\bash\_profile"
 mklink /H "%HOME%\.inputrc" "%HOME%\dotfiles\bash\_inputrc"
@@ -28,6 +32,8 @@ git submodule update
 
 rem シンボリックリンクの作成
 
+del "%HOME%\_vimrc"
+del "%HOME%\_gvimrc"
 mklink /H "%HOME%\_vimrc" "%HOME%\dotfiles\vimfiles\_vimrc"
 mklink /H "%HOME%\_gvimrc" "%HOME%\dotfiles\vimfiles\_gvimrc"
 
