43b84197db816e50fa23366161375ce0d116b420
Ryo Takahashi <rt.sporty@gmail.com>
2013-06-09 18:58:30 +0900

    例外メソッドを追加。ローカル関数に変更。

--- install.py
+++ install.py
@@ -13,79 +13,79 @@ pp = pprint.PrettyPrinter(indent=4)
 
 
 def git():
-    symlink("~/dotfiles/git/_gitignore_global", "~/.gitignore_global")
-    execute('git config --global user.name "Ryo Takahashi"')
-    execute('git config --global user.email rt.sporty@gmail.com')
-    execute('git config --global color.ui auto')
-    execute('git config --global core.pager "less -R"')
+    _symlink("~/dotfiles/git/_gitignore_global", "~/.gitignore_global")
+    _do('git config --global user.name "Ryo Takahashi"')
+    _do('git config --global user.email rt.sporty@gmail.com')
+    _do('git config --global color.ui auto')
+    _do('git config --global core.pager "less -R"')
 
     if sys.platform == "mac":
-        #execute('git config --global core.editor
+        #_do('git config --global core.editor
         #"/Applications/MacVim.app/Contents/MacOS/Vim"')
         pass
 
 
 def python():
     fn = download('http://python-distribute.org/distribute_setup.py')
-    sudo('python ' + fn)
-    sudo('easy_install virtualenv')
-    sudo('easy_install flake8')
+    _sudo('python ' + fn)
+    _sudo('easy_install virtualenv')
+    _sudo('easy_install flake8')
 
 
 def bash():
-    symlink('~/dotfiles/bash/_bashrc', '~/.bashrc')
-    symlink('~/dotfiles/bash/_profile', '~/.profile')
-    symlink('~/dotfiles/bash/_inputrc', '~/.inputrc')
+    _symlink('~/dotfiles/bash/_bashrc', '~/.bashrc')
+    _symlink('~/dotfiles/bash/_profile', '~/.profile')
+    _symlink('~/dotfiles/bash/_inputrc', '~/.inputrc')
 
 
 def vim():
     # submoduleを更新。主にvundle。
-    execute('cd '+np('~/dotfiles'))
-    execute('git submodule init')
-    execute('git submodule update')
+    _do('cd '+_np('~/dotfiles'))
+    _do('git submodule init')
+    _do('git submodule update')
 
     # ドットファイルのシンボリックリンクを作成
-    symlink('~/dotfiles/vimfiles', '~/vimfiles')
+    _symlink('~/dotfiles/vimfiles', '~/vimfiles')
     if sys.platform == "win32":
         prefix = "_"
     else:
         prefix = "."
-    symlink('~/dotfiles/vimfiles/_vimrc', '~/%svimrc' % (prefix, ))
-    symlink('~/dotfiles/vimfiles/_gvimrc', '~/%sgvimrc' % (prefix, ))
+    _symlink('~/dotfiles/vimfiles/_vimrc', '~/%svimrc' % (prefix, ))
+    _symlink('~/dotfiles/vimfiles/_gvimrc', '~/%sgvimrc' % (prefix, ))
 
     # vundleのアップデート
     # TODO: コマンド実行後終了するオプションがほしい
     if sys.platform == "win32":
-        execute('gvim -c "BundleInstall"')
+        _do('gvim -c "BundleInstall"')
     else:
-        execute('vim -c "BundleInstall"')
+        _do('vim -c "BundleInstall"')
     # jediのアップデート
-    execute('cd '+np('~/.vim/bundle/jedi-vim'))
-    execute('git submodule update --init')
+    _do('cd '+_np('~/.vim/bundle/jedi-vim'))
+    _do('git submodule update --init')
 
 
 def powerline():
-    symlink('~/dotfiles/powerline', '~/.config/powerline')
+    _symlink('~/dotfiles/powerline', '~/.config/powerline')
 
 
 def mac():
     if sys.platform == "mac":
         # ターミナル.appの設定をコピー
-        copy('~/Library/Preferences/com.apple.Terminal.plist',
-             '~/dotfiles/mac/com.apple.Terminal.plist.back')
+        _copy('~/Library/Preferences/com.apple.Terminal.plist',
+              '~/dotfiles/mac/com.apple.Terminal.plist.back')
         print "backup plist to mac/ (with extension .back)..."
-        copy('~/dotfiles/mac/com.apple.Terminal.plist',
-             '~/Library/Preferences/')
+        _copy('~/dotfiles/mac/com.apple.Terminal.plist',
+              '~/Library/Preferences/')
         # ファインダーのタイトルバーにパスを表示
-        execute('defaults write com.apple.finder \
+        _do('defaults write com.apple.finder \
                 _FXShowPosixPathInTitle -boolean true')
-        execute('killall Finder')
+        _do('killall Finder')
 
 
 def dump_preferences():
     if sys.platform == "mac":
-        copy('~/Library/Preferences/com.apple.Terminal.plist', 'mac/')
-        execute('plutil -convert xml1 mac/com.apple.Terminal.plist')
+        _copy('~/Library/Preferences/com.apple.Terminal.plist', 'mac/')
+        _do('plutil -convert xml1 mac/com.apple.Terminal.plist')
     else:
         print "dump_preference can use only on mac os."
 
@@ -114,13 +114,13 @@ def all():
 
 
 def test():
-    symlink('./test', '~/test_link')
-    symlink('./test_dir', '~/test_dir_link')
+    _symlink('./test', '~/test_link')
+    _symlink('./test_dir', '~/test_dir_link')
 
 
 if __name__ == "__main__":
 
-    def np(fpath):
+    def _np(fpath):
         """
         pathの正規化
         """
@@ -130,14 +130,14 @@ if __name__ == "__main__":
         fpath = os.path.abspath(fpath)
         return fpath
 
-    def sudo(cmd):
+    def _sudo(cmd):
         """
-        sudoでコマンド実行
+        _sudoでコマンド実行
         TODO: 未実装
         """
-        execute(cmd)
+        _do(cmd)
 
-    def execute(cmd):
+    def _do(cmd):
         """
         コマンド実行
         """
@@ -150,36 +150,36 @@ if __name__ == "__main__":
         if subprocess.call(cmd, shell=True):
             raise Exception("process error. stop installation.")
 
-    def copy(src, dest):
+    def _copy(src, dest):
         """
         コピー作成
         """
         print "cp %s %s" % (src, dest)
         shutil.copyfile(src, dest)
 
-    def symlink(src, dest):
+    def _symlink(src, dest):
         """
         シンボリックリンク作成
         """
 
-        src = np(src)
-        dest = np(dest)
+        src = _np(src)
+        dest = _np(dest)
 
         if sys.platform == "win32":
             # 一旦削除
             if os.path.isdir(dest):
-                execute('RMDIR /Q %s' % (dest, ))
+                _do('RMDIR /Q %s' % (dest, ))
             elif os.path.isfile(dest):
-                execute('DEL /F /Q %s' % (dest, ))
+                _do('DEL /F /Q %s' % (dest, ))
 
             # ジャンクションまたはハードリンク作成
             if os.path.isdir(src):
-                execute("CMD.EXE /C mklink /J /D %s %s" % (dest, src))
+                _do("CMD.EXE /C mklink /J /D %s %s" % (dest, src))
             else:
-                execute("CMD.EXE /C mklink /H %s %s" % (dest, src))
+                _do("CMD.EXE /C mklink /H %s %s" % (dest, src))
         else:
             # 一旦削除
-            execute('rm -f %s' % (dest, ))
+            _do('rm -f %s' % (dest, ))
             # シンボリックリンク作成
             print "ln -s %s %s" % (src, dest)
             os.symlink(src, dest)
@@ -199,7 +199,7 @@ if __name__ == "__main__":
             raise Exception("unsupported scheme.")
 
         if filename:
-            output_filename = np(filename)
+            output_filename = _np(filename)
         else:
             # ヘッダで指定されていたらファイル名を取り出す
             disposition = r1.getheader("Content-Disposition")
@@ -219,7 +219,7 @@ if __name__ == "__main__":
 
     # 使用可能関数の検索
     funcs = {}
-    excepts = ["np", "execute", "copy", "symlink"]
+    excepts = ["_np", "_sudo", "_do", "_copy", "_symlink"]
     mod = inspect.getmodule(all)
     for (name, function) in inspect.getmembers(mod, inspect.isfunction):
         if inspect.isfunction(function) and not name in excepts:
